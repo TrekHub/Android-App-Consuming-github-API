4de863a5da912dac342fe6a392b6406e5fe3b4fb
TrekHub <teddyomondi001@gmail.com>
2022-02-26 22:34:59 +0300

    created the app bar

--- app/src/main/java/com/example/gitsearch/MainActivity.java
+++ app/src/main/java/com/example/gitsearch/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.gitsearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button mGitBtn = (Button) findViewById(R.id.gitBtn);
+        mGitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, DashBoard.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
