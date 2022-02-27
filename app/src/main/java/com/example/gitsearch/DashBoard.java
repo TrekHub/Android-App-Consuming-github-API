a467de26d8d15ed05e9e8e5f26e8871aed8d1413
TrekHub <teddyomondi001@gmail.com>
2022-02-27 11:32:53 +0300

    adding the search bar

--- app/src/main/java/com/example/gitsearch/DashBoard.java
+++ app/src/main/java/com/example/gitsearch/DashBoard.java
@@ -3,6 +3,9 @@ package com.example.gitsearch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class DashBoard extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class DashBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        ImageView mNotification = (ImageView) findViewById(R.id.notification_icon);
+        ImageView mUser = (ImageView) findViewById(R.id.user_icon);
+
+
+        mNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DashBoard.this, "No Notifications!!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        mUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DashBoard.this, "Create a new Profile", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
     }
 }
